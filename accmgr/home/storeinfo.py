43d5a0489bb99bd8beef9d53360dbc085ab5c6e5
Mayank Sachān <otnemio@gmail.com>
2021-10-03 22:30:11 +0530

    Update storeinfo.py

--- accmgr/home/storeinfo.py
+++ accmgr/home/storeinfo.py
@@ -1,8 +1,17 @@
 import hashlib
 from pathlib import Path
 from readpdf import *
-
-h = hashlib.sha256()
+ 
+#function copied from internet
+def hash_file(filename):
+    h_sha256 = hashlib.sha256()
+    with open(filename,'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024) 
+            h_sha256.update(chunk)
+    return h_sha256.hexdigest()
 
 for txt_path in Path(".").glob("*.pdf"):
   print(read_and_tell(txt_path))
+  print(hash_file(txt_path))
